46751346baf68cef5d22a9e58993b06630f6890b
m4xvel <mmenshov2004@gmail.com>
2024-04-03 17:19:20 +0300

    Added information in PagerView

--- composeApp/src/androidMain/kotlin/org/m4xvel/weatherapp/ui/screen/DetailedWeatherScreen.kt
+++ composeApp/src/androidMain/kotlin/org/m4xvel/weatherapp/ui/screen/DetailedWeatherScreen.kt
@@ -1,6 +1,5 @@
 package org.m4xvel.weatherapp.ui.screen
 
-import android.util.Log
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
@@ -11,11 +10,13 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Divider
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.Text
@@ -23,16 +24,16 @@ import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import org.koin.androidx.compose.koinViewModel
+import org.m4xvel.weatherapp.domain.model.Weather
 import org.m4xvel.weatherapp.ui.MainViewModel
+import kotlin.math.roundToInt
 
 @Composable
 fun DetailedWeatherScreen(navController: NavController, mainViewModel: MainViewModel) {
@@ -100,7 +101,11 @@ private fun HorizontalPagerView(mainViewModel: MainViewModel) {
                     },
             ) {
                 Text(
-                    text = "${ mainViewModel.getDayOfMonth(iteration) } ${ mainViewModel.getDayOfWeek(iteration) }",
+                    text = "${mainViewModel.getDayOfMonth(iteration)} ${
+                        mainViewModel.getDayOfWeek(
+                            iteration
+                        )
+                    }",
                     color = textColor,
                     modifier = Modifier.align(alignment = Alignment.Center)
                 )
@@ -108,11 +113,86 @@ private fun HorizontalPagerView(mainViewModel: MainViewModel) {
         }
     }
     HorizontalPager(
-        modifier = Modifier.fillMaxSize(),
         state = pagerState
     ) { page ->
-        Text(
-            text = "${ mainViewModel.getDayOfMonthForecast(page) }"
+        Column(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            Forecast(mainViewModel = mainViewModel, page = page, text = "Температура") { it.temp.roundToInt() }
+            Forecast(mainViewModel = mainViewModel, page = page, text = "Скорость ветра") { it.speed }
+            Forecast(mainViewModel = mainViewModel, page = page, text = "Влажность") { it.humidity }
+            Forecast(mainViewModel = mainViewModel, page = page, text = "Давление") { it.pressure }
+        }
+    }
+}
+
+@Composable
+private fun Forecast(mainViewModel: MainViewModel, page: Int, text: String, value: (Weather) -> Any) {
+    Column(modifier = Modifier.padding(top = 30.dp)) {
+        Text(text = text, fontStyle = FontStyle.Italic, modifier = Modifier.padding(5.dp))
+        Divider(
+            modifier = Modifier.fillMaxWidth()
+                .height(1.dp),
+            color = Color.Black
+        )
+        Row(
+            modifier = Modifier.fillMaxWidth()
+                .height(80.dp),
+            horizontalArrangement = Arrangement.SpaceAround,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            ForecastData(
+                mainViewModel = mainViewModel,
+                page = page,
+                text = "Ночь",
+                time = 0
+            ) {
+                value(it)
+            }
+            ForecastData(
+                mainViewModel = mainViewModel,
+                page = page,
+                text = "Утро",
+                time = 6
+            ) {
+                value(it)
+            }
+            ForecastData(
+                mainViewModel = mainViewModel,
+                page = page,
+                text = "День",
+                time = 12
+            ) {
+                value(it)
+            }
+            ForecastData(
+                mainViewModel = mainViewModel,
+                page = page,
+                text = "Вечер",
+                time = 18
+            ) {
+                value(it)
+            }
+        }
+        Divider(
+            color = Color.Black,
+            modifier = Modifier.height(1.dp)
+                .fillMaxWidth()
+        )
+    }
+}
+
+@Composable
+private fun ForecastData(mainViewModel: MainViewModel, page: Int, text: String, time: Int, value: (Weather) -> Any) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = text)
+        Text(text = mainViewModel.getDayOfMonthForecast(page)
+            ?.filter { it.key == time }
+            ?.flatMap { it.value }
+            ?.map { value(it) }
+            ?.joinToString("") ?: ""
         )
     }
 }
